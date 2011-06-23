4fb1cd68a5e843ea50477bcfef354b1d6fd7e179
Thaisan Tonthat <tonthat@dinosaur.local>
2011-06-23 16:53:13 -0700

    initial commit

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,38 @@
+class SiteController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def search
+    api_url = "http://:0gV8oFvfEuktVu@defgo.api.indextank.com"
+    api = IndexTank::Client.new api_url
+    index = api.indexes "howscharlie"
+    index.functions(0, "-doc.var[0]").add
+    
+    results = index.search(params["query"], :function => 0, :fetch => "*");
+
+    @num_matches = results['matches']
+    @search_results = results['results']
+    
+    @dates = @search_results.collect { |s| [s["date"], Time.at(s["date"].to_i).strftime("%m/%d/%Y")] }
+    @sentiment = @search_results.collect { |s| s["sentiment"] }
+    @last_sentiment = @sentiment.last.to_f
+    
+    #render :text => @dates
+    
+    if @last_sentiment < -2 then 
+      @how_is = "bad"
+      @how_is_class = "doing_bad"
+    elsif @last_sentiment > -2 && @last_sentiment < 2 
+      @how_is = "ok"
+      @how_is_class = "doing_ok"
+    else
+      @how_is = "good"
+      @how_is_class = "doing_good"
+    end
+    
+    @latest_date = @dates.last[1]
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Howscharlie::Application.config.secret_token = 'f11b5b399d09e82a7d1eab693aa8ce2eeb63fc486562f7785d0acf468f687a849e616631c645de58ee8c7e0e888c8cc11d63a07729140944f5ed5787e2cc2053'
